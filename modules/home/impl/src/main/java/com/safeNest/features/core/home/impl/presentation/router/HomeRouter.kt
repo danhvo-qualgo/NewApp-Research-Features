58608505a65c13c7c2d4fc91eb134b549d7de2ed
Kiet Luong <kiet.lt@qualgo.net>
2026-02-04 11:04:39 +0700

    Update

--- modules/home/impl/src/main/java/com/safeNest/features/core/home/impl/presentation/router/HomeRouter.kt
+++ modules/home/impl/src/main/java/com/safeNest/features/core/home/impl/presentation/router/HomeRouter.kt
@@ -5,23 +5,13 @@ import android.content.Intent
 import android.net.Uri
 import com.uney.core.router.Router
 import com.safeNest.features.core.home.impl.presentation.HomeActivity
+import com.uney.core.router.InternalRouter
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
-private const val DEEPLINK_SCHEME = "internal"
 private const val DEEPLINK_HOST = "home"
 
 class HomeRouter @Inject constructor(
     @param:ApplicationContext
     private val context: Context
-) : Router() {
-    override fun canHandle(deeplink: Uri): Boolean {
-        return deeplink.scheme == DEEPLINK_SCHEME && deeplink.host == DEEPLINK_HOST
-    }
-
-    override fun getLaunchIntent(deeplink: Uri): Intent {
-        return Intent(context, HomeActivity::class.java).apply {
-            data = deeplink
-        }
-    }
-}
+) : InternalRouter(context, DEEPLINK_HOST, HomeActivity::class)
