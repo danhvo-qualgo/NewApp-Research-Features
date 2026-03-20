b59b2c75b116f74d015d6ef306dc48c1278ccfb7
Kiet Luong <kiet.lt@qualgo.net>
2026-03-20 11:06:34 +0700

    Remove phishing module

--- modules/phishingDetection/impl/src/main/java/com/safeNest/demo/features/phishingDetection/impl/presentation/PhishingDetectionProviderImpl.kt
+++
@@ -1,6 +0,0 @@
-package com.safeNest.demo.features.phishingDetection.impl.presentation
-
-import com.safeNest.demo.features.phishingDetection.api.PhishingDetectionProvider
-import javax.inject.Inject
-
-class PhishingDetectionProviderImpl @Inject constructor() : PhishingDetectionProvider
